9e8a1a58cfbe609b841a4d8b44a172a19be2f555
afahmed <ahmedkadar.farah@gmail.com>
2019-10-19 16:15:23 -0500

    first lesson

---
+++ src/firstLesson/Exmaple.java
@@ -0,0 +1,71 @@
+package firstLesson;
+
+public class Exmaple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// this is comment
+		
+		// variables
+		// variables hold data
+		
+		// we want hold data of 
+		
+		// dataype variable_name
+		int number;
+		number = 10;
+		int numbers = 15;
+		number = 12;
+		number = 13;
+		System.out.println(number);
+		System.out.println(numbers);
+		
+		double numberDouble = 2.5;
+		//numberDouble = 2;
+		System.out.println(numberDouble);
+		
+		//bank balance
+		double balance = 100;
+		System.out.println("initial balance is $" + balance);
+		
+		// deposited $40
+		balance = balance + 40;
+		System.out.println(balance);
+		
+		balance = balance - 20;
+		System.out.println(balance);
+		
+		char c = '3';
+		int num = 3;
+		
+		String name = "Ahmed";
+		System.out.println("Hello, my name is " + name);
+		
+		//non-primitive
+		//I want a variable that holds students name
+		
+		String studentFirstName = " Ahmed ";
+		System.out.println("My name is " + studentFirstName);
+		
+		String studentLastName = "Farah";
+		System.out.println("student full name is" + studentFirstName+ studentLastName);
+
+		
+		//initialize and create variable called balance to zero
+		
+		double balance2 = 0;
+		
+		//customer deposits 100 in the bank
+		
+		double deposit = 100;
+		
+		balance2 = balance2 + deposit;
+		System.out.println("new balance = $" + balance2);
+		
+		
+		
+	
+	}
+
+}
