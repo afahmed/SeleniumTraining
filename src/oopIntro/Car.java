72af7d80f71b0192c1e4e66de2b45d80add35f30
afahmed <ahmedkadar.farah@gmail.com>
2019-12-21 17:55:04 -0600

    constructors

--- src/oopIntro/Car.java
+++ src/oopIntro/Car.java
@@ -1,14 +1,29 @@
 package oopIntro;
 
-public class Car {
+public class Car { // start of class
 	
 	// variables and/or methods
+	// encapsulation 
+	// access modifiers => public private protected
+	// read ahead about getters and constructors
+	private int doors;
+	private int wheels;
+	private String color;
+	private String model;
+	private int speed = 0;
 	
-	int doors;
-	int wheels;
-	String color;
-	String model;
-	int speed = 0;
+	
+	
+	public Car(int doors, int wheels, String color, String model, int speed) {
+		
+		this.doors = doors;
+		this.wheels = wheels;
+		this.color = color;
+		this.model = model;
+		this.speed = speed;
+		
+//		System.out.println("constructor called!!!");
+	}
 
 	public void carStatus() {
 //		int speed = 0;
@@ -18,5 +33,51 @@ public class Car {
 			System.out.println("The car is moving at " + speed + "mph");
 		}
 	}
+	
+	// getters and setters
+	// setters
+	public void setDoors(int numberOfDoors) {
+		// logic
+		doors = numberOfDoors;
+	}
+	
+	public void setWheels(int numberOfWheels) {
+		wheels = numberOfWheels;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	//getters
+	
+	public int getDoors() {
+		return doors;
+	}
+	
+	public int getWheels() {
+		return this.wheels;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
 
-}
+} // end of class
--- src/oopIntro/MainClass.java
+++ src/oopIntro/MainClass.java
@@ -4,21 +4,69 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		
+//		int x;
+//		// instantiation
+//		Car car = new Car();
+//		
+////		car.model = "Honda Accord";
+////		car.color = "Gray";
+////		car.doors = 4;
+//		car.setDoors(2);
+////		car.wheels = 4;
+////		System.out.println(car.model);
+////		System.out.println(car.doors);
+////		System.out.println("The car is moving at " + car.speed + "mph");
+//		car.carStatus();
+//		
+//		car.setColor("Gray");
+//		
+////		car.speed = 75;
+//		
+//		car.carStatus();
+//		
+//		System.out.println("The car has " + car.getDoors() + " doors");
+//		
+//		Car car2 = new Car();
+//		
+//		car2.setDoors(4);
+//		System.out.println("The car2 has " + car2.getDoors() + " doors");
+//		
+//		// set the color for car2
+//		car2.setColor("Black");
+//		
+//		// print the value of color in car2
+//		System.out.println(car2.getColor());
+//		
+//		// set the car model
+//		
+//		
+//		// print out the model to the console
 		
-		int x;
-		Car car = new Car();
 		
-		car.model = "Honda Accord";
-		car.color = "Gray";
-		car.doors = 4;
-		car.wheels = 4;
-		System.out.println(car.model);
-//		System.out.println("The car is moving at " + car.speed + "mph");
-		car.carStatus();
+		// constructors
 		
-		car.speed = 75;
+		Car honda = new Car(4, 4, "Black", "Accord", 0);
+		Car toyota = new Car(2, 4, "White", "Camry", 0);
 		
-		car.carStatus();
+//		System.out.println(honda.getModel());
+		
+//		honda.setModel("Civic");
+		System.out.println(honda.getModel());
+		System.out.println(toyota.getModel());
+		
+		
+		/**
+		 * 	create a class called bank
+		 * 	the class should have bank name, customer name, customer balance
+		 * 	the Bank class should have the following methods
+		 * 		deposit()
+		 * 		withdraw()
+		 * 		checkBalance()
+		 * 
+		 * 	use scanner class to get input from the keyboard
+		 * 		hint: please refer to doWhileLoo.java class in loops package
+		 */
 
 
 	}
